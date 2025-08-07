1531127cd36f982d8567d350d7743a4f01fb13aa
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-08-07 01:48:50 +0100

    added one inch wallet header  and other things into it

--- src/Components/OneinchProuducts.jsx
+++ src/Components/OneinchProuducts.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import Port2 from "../Components/Images/Port2.png";
+import Phoneimg from "../Components/Images/Phoneimg.png";
+import Button from "./Button";
 
 const OneinchProuducts = () => {
   return (
     <>
-      <div className=" text-center text-[40px]  relative top-[100px] text-[white]">
-        <b>
-          <p>1inch products</p>
+      <div className="mt-[50px] inchwalletheading flex justify-center items-center">
+        <b className="text-center text-[45px] text-[white] ">
+          <p>1inch Wallet</p>
         </b>
+      </div>
+      <div className="  flex text-[white]">
+        <div className=" items-center justify-center   p-[20px] flex ">
+          <div className=" w-[100%]  rounded-[20px] omneinchwallet p-[20px] flex justify-around items-center">
+            <div className=" p-[15px]">
+              <b className="text-center text-[45px] flex justify-start">
+                <p>1inch Wallet</p>
+              </b>
+              <p className="text-[14px] text-[#fff9]">
+                The most powerful mobile app for managing your assets and
+                exploring Web3.
+              </p>
+              <div className="flex mt-[55px] oneinchwalletsmallscreen  w-[50%] justify-between">
+                <Button className=" " props="Download" />
+                <Button
+                  className="bg-[transparent]  border border-amber-50"
+                  props="Learn more"
+                />
+              </div>
+            </div>
+            <img className="0px]" src={Phoneimg} />
+          </div>
+        </div>
       </div>
     </>
   );
