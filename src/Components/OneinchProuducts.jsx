c3698aa3794915ea9df88344a0b0cd3f4f4ad5e9
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-08-07 02:22:39 +0100

    added more oneinch portfolio contents

--- src/Components/OneinchProuducts.jsx
+++ src/Components/OneinchProuducts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Port2 from "../Components/Images/Port2.png";
 import Phoneimg from "../Components/Images/Phoneimg.png";
+import Oneinchcard from "../Components/Images/Oneinchcard.png";
 import Button from "./Button";
 
 const OneinchProuducts = () => {
@@ -54,6 +55,28 @@ const OneinchProuducts = () => {
                 />
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="  flex text-[white]">
+        <div className=" items-center justify-center   w-[100%] p-[10px] flex ">
+          <div className=" w-[100%]  rounded-[20px] omneinchwallet p-[20px] flex justify-around items-center">
+            <div className=" p-[15px] ">
+              <b className="text-center text-[45px] flex justify-start">
+                <p>1inch Card</p>
+              </b>
+              <p className="text-[14px] text-[#fff9]">
+                A physical & virtual card for digital assets.
+              </p>
+              <div className="flex mt-[55px] oneinchwalletsmallscreen   w-[100%] justify-between">
+                <Button className=" " props="Order card" />
+                <Button
+                  className="bg-[transparent]  border border-amber-50"
+                  props="Learn more"
+                />
+              </div>
+            </div>
+            <img className="portfolioimg" src={Oneinchcard} />
           </div>
         </div>
       </div>
