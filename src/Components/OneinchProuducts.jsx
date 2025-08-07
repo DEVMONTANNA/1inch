df01e1b8177d61864cfb70ac8f98f9264a8c224d
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-08-07 02:02:59 +0100

    added one inch wallet header  and other things into it

--- src/Components/OneinchProuducts.jsx
+++ src/Components/OneinchProuducts.jsx
@@ -6,15 +6,15 @@ import Button from "./Button";
 const OneinchProuducts = () => {
   return (
     <>
-      <div className="mt-[50px] inchwalletheading flex justify-center items-center">
+      <div className="mt-[55px] inchwalletheading flex justify-center items-center">
         <b className="text-center text-[45px] text-[white] ">
-          <p>1inch Wallet</p>
+          <p>1inch Portfolio</p>
         </b>
       </div>
-      <div className="  flex text-[white]">
-        <div className=" items-center justify-center   p-[20px] flex ">
+      <div className=" ] flex text-[white]">
+        <div className=" items-center justify-center   w-[100%] p-[20px] flex ">
           <div className=" w-[100%]  rounded-[20px] omneinchwallet p-[20px] flex justify-around items-center">
-            <div className=" p-[15px]">
+            <div className=" p-[15px] ">
               <b className="text-center text-[45px] flex justify-start">
                 <p>1inch Wallet</p>
               </b>
@@ -22,7 +22,7 @@ const OneinchProuducts = () => {
                 The most powerful mobile app for managing your assets and
                 exploring Web3.
               </p>
-              <div className="flex mt-[55px] oneinchwalletsmallscreen  w-[50%] justify-between">
+              <div className="flex mt-[55px] oneinchwalletsmallscreen   w-[50%] justify-between">
                 <Button className=" " props="Download" />
                 <Button
                   className="bg-[transparent]  border border-amber-50"
